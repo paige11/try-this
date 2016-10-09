52bb7eaab60db3221a1a4f0b04491ceacff74b4c
Flatiron School <ellis.paige.v@gmail.com>
2016-10-09 15:02:54 -0400

    added validation for presence of name to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   has_many :category_questions
   has_many :questions, through: :category_questions
   scope :alphabetize, -> { order(:name) }
-
+  validates_presence_of :name
 
 end
