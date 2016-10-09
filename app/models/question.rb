d5107aa6339b7ac672d6d89eecbb5d8e0040c5f9
Flatiron School <ellis.paige.v@gmail.com>
2016-10-09 15:56:05 -0400

    added categories.clear to Question#category_ids= so that updating would not include previous categories that user unchecked while editing

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,7 @@ class Question < ApplicationRecord
   has_many :category_questions
   has_many :categories, through: :category_questions
   validates_presence_of :content
+  validates_uniqueness_of :content
   scope :most_recent, -> { order(created_at: :desc) }
   scope :ten_most_recent, -> { most_recent.limit(10) }
 
@@ -13,6 +14,7 @@ class Question < ApplicationRecord
   end
 
   def category_ids=(category_ids)
+    categories.clear
     category_ids.each do |cid|
       if !cid.empty?
         category = Category.find(cid)
