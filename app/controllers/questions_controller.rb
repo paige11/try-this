6abeba115b42f7164b960da9c8a1f793e955a1ad
Flatiron School <ellis.paige.v@gmail.com>
2016-10-09 01:33:37 -0400

    added actions to controllers without definitions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,22 @@
 class QuestionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
-  
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
   def show
   end
+
+  def destroy
+  end
+  
 end
