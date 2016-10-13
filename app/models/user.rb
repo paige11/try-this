d9eee21ce787d3f0993bef17170ce5123a11dfcc
Flatiron School <ellis.paige.v@gmail.com>
2016-10-13 11:26:41 -0400

    added commented-out possibility for fb user's name to self.from_omniauth in User - return to this later

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,11 @@ class User < ApplicationRecord
   include ApplicationHelper
 
   def self.from_omniauth(auth)
+    byebug
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+      # user.name = auth.info.name ADD LATER - FOR FB USERS TAKE NAME, OTHERWISE UNAVAIL?
     end
   end
 
